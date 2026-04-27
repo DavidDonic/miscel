4939b67cd6d81e6d264771f6bab8af925078e7c8
DavidDonic <2358david@gmail.com>
2026-04-27 00:09:53 -0400

    shopping cart-OOD-processing

---
+++ OOD/src/main/java/dado/lab/large_spaces/ShoppingCart.java
@@ -0,0 +1,12 @@
+package dado.lab.large_spaces;
+
+public class ShoppingCart {
+    // --- objects ---
+    // --- opt1: system -> carts -> goods ---
+    // --- #opt2: cart -> shops -> goods ---
+    // --- functions: ---
+    //     listing / unlisting
+    //     checking -> selective checking
+    //     *coupon
+    //     *managing (views of cart) -> filter ---
+}
