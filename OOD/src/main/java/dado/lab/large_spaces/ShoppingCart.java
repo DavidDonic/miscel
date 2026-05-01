bc672f40d408bd0a945fcd87c7e53287cedb3ac9
DavidDonic <2358david@gmail.com>
2026-04-30 23:45:41 -0400

    shopping cart - classes & fields finished

--- OOD/src/main/java/dado/lab/large_spaces/ShoppingCart.java
+++ OOD/src/main/java/dado/lab/large_spaces/ShoppingCart.java
@@ -1,12 +1,47 @@
 package dado.lab.large_spaces;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShoppingCart {
-    // --- objects ---
-    // --- opt1: system -> carts -> goods ---
-    // --- #opt2: cart -> shops -> goods ---
-    // --- functions: ---
-    //     listing / unlisting
-    //     checking -> selective checking
-    //     *coupon
-    //     *managing (views of cart) -> filter ---
+    // --- shopping cart ---
+    // --- features ---
+    // ->***addProduct()
+    // ->oneMore()
+    // ->oneLess()
+    // ->***deleteProduct()
+    // ->***checkAll()
+    // ->checkSelected()
+    // --- instances ---
+    class Product {
+        private String name;
+        int price;
+
+        Product(int pri, String name) {
+            this.price = pri;
+            this.name = name;
+        }
+
+    }
+    class CartItem {
+        //add-ons: Unit (kg/amount/length... ->enum)
+        final Product product;
+        private int quantity;
+
+        CartItem(Product product, int quantity) {
+            this.product = product;
+            this.quantity = quantity;
+        }
+
+        oneMore() {}
+        oneLess(){}
+    }
+    class Cart {
+        private Map<String, CartItem> items = new HashMap<>();
+        addProduct(){}
+        deleteProduct(){}
+        checkAll(){}
+        checkSelected(){}
+    }
+
 }
